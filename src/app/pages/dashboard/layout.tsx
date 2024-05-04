117adeb0b5c2880b469932e97d5df4007e9717c3
aidev <159957293+dev-aiwebsite@users.noreply.github.com>
2024-05-05 00:46:18 +0800

    Update dashboard layout.tsx

--- src/app/pages/dashboard/layout.tsx
+++ src/app/pages/dashboard/layout.tsx
@@ -3,7 +3,11 @@ import Sidebar from "@/components/sidebar/sidebar"
 
 
 
-const layout = ({children}) => {
+const layout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
  return <div className="h-screen flex flex-col">
       <Navbar/>
    <div className="flex-1 flex flex-row">
