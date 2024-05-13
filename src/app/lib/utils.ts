80dbc4cb58ed45e820e98982713c36a066546d3c
aidev <dev@aiwebsiteservices.com>
2024-05-13 19:33:27 +0800

    update db connection

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose"
+import mongoose, { ConnectOptions } from "mongoose"
 
 export const connectToDb = async () => {
     const connection = {
@@ -8,7 +8,10 @@ export const connectToDb = async () => {
         if (!process.env.MONGODB_URI) {
             throw new Error("MongoDB URI is not defined");
         }
-        const db = await mongoose.connect(process.env.MONGODB_URI)
+        const db = await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        } as ConnectOptions);
         connection.isConnected = db.connections[0].readyState
         connection.isConnected ? console.log('connected') : console.log('not connected')
     } catch (err:any) {
