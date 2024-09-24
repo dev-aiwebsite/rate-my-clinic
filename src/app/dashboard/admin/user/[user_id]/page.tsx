0708809966f064720f3d77da7724577800513190
aidev <dev@aiwebsiteservices.com>
2024-09-24 15:26:39 +0800

    add restriction admin page

--- src/app/dashboard/admin/user/[user_id]/page.tsx
+++ src/app/dashboard/admin/user/[user_id]/page.tsx
@@ -26,13 +26,10 @@ type TsurveyData = {
 } | null
 export default function Page({params}:{params:any}){
         const {currentUser, users} = useSessionContext()
-        const router = useRouter();
-        if (currentUser.role != 'admin') {
-            router.push("/dashboard"); // Redirect to /dashboard
-            return null; // Return null to prevent further rendering
-        }
         const [surveyData, setSurveyData] = useState<TsurveyData>(null)
-
+        const router = useRouter();
+      
+        
             useEffect(()=> {
                 if(surveyData) return
                 let user = users.find((i: { _id: string }) => i._id == '66b48dd97f01ae86ade504cb')
@@ -47,7 +44,10 @@ export default function Page({params}:{params:any}){
                 getSD()
             },[])
         
-    
+            if (currentUser.role != 'admin') {
+                router.push("/dashboard"); // Redirect to /dashboard
+                return null; // Return null to prevent further rendering
+            }
         return <>
          <Link className="m-2 block w-fit rounded-lg bg-transparent hover:bg-gray-100 p-2"
                  href={"/dashboard/admin"}><IoIosArrowRoundBack size={32} /></Link>
